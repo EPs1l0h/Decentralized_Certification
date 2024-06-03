1c4887282db47d6d6b839325d3e79151ec9b777b
Windy <wdwdwd@bupt.edu.cn>
2024-06-03 15:47:58 +0800

    commit

--- Frontend/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-import "@/assets/global.css";
-import router from "./router";
-import store from "./store";
-createApp(App).use(ElementPlus).use(store).use(router).mount("#app");
