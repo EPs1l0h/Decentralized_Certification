4201058a64ff3fc3d57336382229ea1f9d8b6a21
Windy <wdwdwd@bupt.edu.cn>
2024-06-03 15:45:17 +0800

    frontend init

---
+++ ca-frontend/generated/models/PostAddRequest.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type PostAddRequest = {
+    content?: string;
+    tags?: Array<string>;
+    title?: string;
+};
+
