85d8144e83f1b5af1ebb745d2f89e86592935c83
EPsilon <97523324+EPs1l0h@users.noreply.github.com>
2024-06-08 01:52:14 +0800

    ultimate-backend

--- Issuer/app.py
+++ Issuer/app.py
@@ -3,7 +3,7 @@ import sqlite3
 import json
 import os
 from datetime import datetime, timezone
-
+from flask_cors import cross_origin
 from web3 import Web3
 from flask import Flask, request, jsonify
 import requests
@@ -81,13 +81,9 @@ def init_func():
     from sqlite import init_sqlite
     init_sqlite()
 
-<<<<<<< Updated upstream
-@app.route('/checkUserName', methods=['GET'])
-=======
 
 @app.route('/checkUserName', methods=['POST'])
 @cross_origin()
->>>>>>> Stashed changes
 def checkUserName():
     """
     轮询，返回是否在线
@@ -112,6 +108,7 @@ def checkUserName():
 
 # 和持有者交互 - 注册
 @app.route('/register', methods=['POST'])
+@cross_origin()
 def register():
     """
     data = request.get_json()
@@ -120,23 +117,17 @@ def register():
     """
     print("========== 开始执行 register 函数 ==========")
     data = request.json
-<<<<<<< Updated upstream
-=======
     print("接收到前端的数据:")
     print(json.dumps(data, indent=4))
 
     global is_login, user_name
     user_name = data.get('username')
     is_login = True
->>>>>>> Stashed changes
     password = data.get('password')
     password_confirm = data.get('password_confirm')
+    algorithm = data.get('algorithm')
 
     if password != password_confirm:
-<<<<<<< Updated upstream
-        return jsonify({"message": "Registration Failed"}), 401
-    registerDID()
-=======
         response_data = {"message": "Registration Failed"}
         print("返回给前端的数据:")
         print(json.dumps(response_data, indent=4))
@@ -153,7 +144,6 @@ def register():
     conn.commit()
     conn.close()
 
->>>>>>> Stashed changes
     # 返回
     response_data = {"message": "Registration Successful"}
     print("返回给前端的数据:")
@@ -164,6 +154,7 @@ def register():
 
 # 获取Holder信息
 @app.route('/get_holder_info', methods=['GET'])
+@cross_origin()
 def get_holder_info():
     """
     获取 username 和 did
@@ -199,6 +190,7 @@ def get_holder_info():
 
 # 和持有者交互 - 登录
 @app.route('/login', methods=['POST'])
+@cross_origin()
 def login():
     """
     data = request.get_json()
@@ -232,6 +224,7 @@ def login():
 
 
 @app.route('/requestAuthenticateDID', methods=['POST'])
+@cross_origin()
 def requestAuthenticateDID():
     """
     持有者发给颁发者DID
@@ -260,6 +253,7 @@ def requestAuthenticateDID():
 
 
 @app.route('/holderRequestVC', methods=['POST'])
+@cross_origin()
 def holderRequestVC():
     """
     持有者返回已有的VC给前端
@@ -296,6 +290,7 @@ def holderRequestVC():
 
 
 @app.route('/getBeAskedVPList', methods=['POST'])
+@cross_origin()
 def getBeAskedVPList():
     """
     持有者返回 VP 请求给前端.
@@ -323,7 +318,8 @@ def getBeAskedVPList():
         return jsonify(response_data), 200
 
 
-@app.route('holderToVerifier', methods=['POST'])
+@app.route('/holderToVerifier', methods=['POST'])
+@cross_origin()
 def holderToVerifier():
     """
     持有者生成VP发给验证者
@@ -375,6 +371,7 @@ def holderToVerifier():
 
 
 @app.route('/recVerifyRequest', methods=['POST'])
+@cross_origin()
 def recVerifyRequest():
     """
     持有者就收验证者请求存到数据库
@@ -406,6 +403,7 @@ being_verified_vp = None
 
 
 @app.route('/getVP', methods=['POST'])
+@cross_origin()
 def getVP():
     """
     验证者接收VP
@@ -420,6 +418,7 @@ def getVP():
 
 
 @app.route('/checkVP', methods=['POST'])
+@cross_origin()
 def checkVP():
     """
     返回给验证者前端 VP
@@ -427,10 +426,6 @@ def checkVP():
     print("========== 开始执行 checkVP 函数 ==========")
     global being_verified_vp
     if being_verified_vp:
-<<<<<<< Updated upstream
-        return jsonify({'vp': being_verified_vp})
-@app.route('verifyVP', methods=['POST'])
-=======
         response_data = {'vp': being_verified_vp}
         print("返回给前端的数据:")
         print(json.dumps(response_data, indent=4))
@@ -442,7 +437,6 @@ def checkVP():
 
 @app.route('/verifyVP', methods=['POST'])
 @cross_origin()
->>>>>>> Stashed changes
 def verifyVP():
     """
     验证者验证VP
@@ -467,6 +461,7 @@ def get_current_time():
 
 
 @app.route('/askHolder', methods=['POST'])
+@cross_origin()
 def askHolder():
     """
     验证者给给持有者发送请求
@@ -498,14 +493,10 @@ def askHolder():
 
 
 being_verified_did = None
-<<<<<<< Updated upstream
-@app.route('/recDID',  methods=['POST'])
-=======
 
 
 @app.route('/recDID', methods=['POST'])
 @cross_origin()
->>>>>>> Stashed changes
 def recDID():
     """
     颁发者接收DID
@@ -520,6 +511,7 @@ def recDID():
 
 
 @app.route('/getWhoRequestVerifyDID', methods=['POST'])
+@cross_origin()
 def getWhoRequestVerifyDID():
     """
     颁发者把DID发给前端
@@ -546,22 +538,15 @@ def getWhoRequestVerifyDID():
             return jsonify(response_data)
 
 
-@app.route('/registerDID', methods=['POST'])
-def registerDID():
+def registerDID(algorithm):
     """
     注册DID
     """
     print("========== 开始执行 registerDID 函数 ==========")
     global contract_addr
-<<<<<<< Updated upstream
-    # data = request.json
-    # type_of_key = data.get('type_of_key') # 一个list，每项用来选择SM2 or RSA
-    type_of_key = ['RSA']
-=======
     type_of_key = []
     type_of_key.append(algorithm)
     print("选择的算法类型:", type_of_key)
->>>>>>> Stashed changes
     public_key_pem = []
     private_key_pem = []
     from web3src.generate_key import generate_keys
@@ -599,6 +584,7 @@ def registerDID():
 
 # 和颁发者交互
 @app.route('/reqeustVerifyDID', methods=['POST'])
+@cross_origin()
 def reqeustVerifyDID():
     """
     颁发者验证DID
@@ -639,6 +625,7 @@ def reqeustVerifyDID():
 
 
 @app.route('/giveVCToHolder', methods=['POST'])
+@cross_origin()
 def giveVCToHolder():
     """
     颁发者颁发VC
@@ -699,6 +686,7 @@ def giveVCToHolder():
 
 # 和验证者交互 - 验证VC
 @app.route('/verifyVC', methods=['POST'])
+@cross_origin()
 def verifyVC():
     print("========== 开始执行 verifyVC 函数 ==========")
     """
@@ -787,15 +775,4 @@ def verifyVP(data):
 
 if __name__ == '__main__':
     init_func()
-<<<<<<< Updated upstream
-    # 把下面函数的注释去掉，再把app.run注释掉可以调试运行
-    registerDID()
-    verifyDID()
-    generateVC()
-    verifyVC()
-    generateVP()
-    verifyVP()
-    # app.run(debug=True)
-=======
     app.run(host='10.21.246.227', debug=True, port=5000)
->>>>>>> Stashed changes
